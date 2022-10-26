0bb08cfb8e0d8532d995b7bca9c03fd5c0f9dcbe
AbdulkadirCELEBI <45809955+AbdulkadirCELEBI@users.noreply.github.com>
2022-10-26 07:20:56 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 21 12:01:45 2021
+
+@author: celebi
+"""
+
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['drowsiness_detect.py'])
